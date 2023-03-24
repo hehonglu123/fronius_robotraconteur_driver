ce6066b01fc97aacd4e54c04fbf727e03659684a
John Wason <wason@wasontech.com>
2023-03-24 14:56:06 -0400

    Add commands and modbus send

--- src/fronius_robotraconteur_driver/fronius_welder_modbus.py
+++ src/fronius_robotraconteur_driver/fronius_welder_modbus.py
@@ -28,6 +28,27 @@ def _uint16_to_float(u16_val, scale, signed = True):
     else:
         return float(u16_val) / float(scale)
 
+def _sint16_to_uint16(s16_val):
+    if s16_val >= 0:
+        assert s16_val <= 0x7FFF, "Value out of range"
+        return s16_val
+    else:
+        assert s16_val >= -0x8000, "Value out of range"
+        return 0x10000 + s16_val
+
+def _clamp_uint16(u16_val):
+    if u16_val < 0:
+        return 0
+    if u16_val > 0xFFFF:
+        return 0xFFFF
+    return u16_val
+
+def _float_to_uint16(float_val, scale, signed = True):
+    if signed:
+        return _sint16_to_uint16(int(float_val*scale))
+    else:
+        return _clamp_uint16(int(float_val*scale))
+
 def _bit(b):
     return 1 << b
 
@@ -43,6 +64,7 @@ class WelderImpl:
         self.device_info = device_info
 
         self._welder_ip = welder_ip
+        self._monitor_only = monitor_only
 
         self._seqno = 0
 
@@ -62,6 +84,26 @@ class WelderImpl:
         self._keep_going = False
         self._thread = None
 
+        self._robot_ready = False
+        self._start_weld = False
+        self._job_number = 0
+        self._program_number = 0
+        self._gas_on = False
+        self._wire_inching = False
+        self._wire_retract = False
+        self._torch_blowout = False
+        self._touch_sensing = False
+        self._error_reset = False
+
+        self._arc_length_correction = 0.0
+        self._puls_dynamik_correction = 0.0
+        self.seam_number = 0
+        self._penetration_stabilizer = 0.0
+        self._arc_length_stabilizer = 0.0
+
+
+        self._command_lock = threading.Lock()
+
     def RRServiceObjectInit(self, ctx, service_path):
         self._downsampler = RR.BroadcastDownsampler(ctx)
         self._downsampler.AddWireBroadcaster(self.device_clock_now)
@@ -76,6 +118,17 @@ class WelderImpl:
 
     def _stop(self):
         assert self._keep_going
+
+        self._robot_ready = False
+        self._start_weld = False
+        self._gas_on = False
+        self._wire_inching = False
+        self._wire_retract = False
+        self._torch_blowout = False
+        self._touch_sensing = False
+
+        time.sleep(1)
+
         with self._lock:
             self._keep_going = False
         self._thread.join()
@@ -88,9 +141,14 @@ class WelderImpl:
                 modbus.connect()
 
                 while self._keep_going:
-                    read_res = modbus.read_holding_registers(0xF100,30)
-
-                    read_regs = [read_res.getRegister(i) for i in range(30)]
+                    if self._monitor_only:
+                        read_res = modbus.read_holding_registers(0xF100,30)
+                        read_regs = [read_res.getRegister(i) for i in range(30)]
+                    else:
+                        control_reg = self._current_command_to_regs()
+                        read_res = modbus.readwrite_registers(0xF100, 30, 0xF000, write_registers=control_reg[0:30])
+                        assert(read_res.function_code < 0x80)
+                        read_regs = read_res.registers
 
                     state_struct = self._welder_modbus_regs_to_state(read_regs)
 
@@ -178,7 +236,168 @@ class WelderImpl:
         ret.wire_position = _uint16_to_float(regs[0x13],100)
 
         return ret
+
+    def _current_command_to_regs(self):
+        
+        regs = [0]*32
+
+        control_flags2 = 0
+        if self._robot_ready:
+            control_flags2 |= _bit(1)
+
+        if self._start_weld:
+            control_flags2 |= _bit(0)
+
+        if self._gas_on:
+            control_flags2 |= _bit(3)
+
+        if self._error_reset:
+            control_flags2 |= _bit(2)
+
+        if self._wire_inching:
+            control_flags2 |= _bit(4)
+
+        if self._wire_retract:
+            control_flags2 |= _bit(5)
+
+        if self._torch_blowout:
+            control_flags2 |= _bit(6)
+
+        if self._touch_sensing:
+            control_flags2 |= _bit(8)
+
+        # TODO wire_sense
+        regs[1] = control_flags2
+
+        # working mode
+        # TODO use parameter
+        regs[8] = 2
+
+        regs[9] = self._job_number
+        regs[0xA] = self._program_number
+
+        regs[0xB] = 0
+        with suppress(RR.ValueNotSetException,AttributeError):
+            regs[0xB] = _float_to_uint16(self.wire_feeder_command.InValue[0],100)
+
+        with suppress(RR.ValueNotSetException,AttributeError):
+            regs[0xF] = _float_to_uint16(self.welding_speed.InValue[0],10,False)
+
+        return regs
     
+    def prepare_welder(self):
+        with self._command_lock:
+            self._robot_ready = True
+            time.sleep(0.1)
+
+    def release_welder(self):
+        self._robot_ready = False
+        self._start_weld = False
+
+    def start_weld(self):
+        with self._command_lock:
+            self._start_weld = True
+    
+    def stop_weld(self):
+        self._start_weld = False
+
+    def start_touch_sensing(self):
+        with self._command_lock:
+            self._touch_sensing = True
+
+    def stop_touch_sensing(self):
+        self._touch_sensing = False
+
+    @property
+    def job_number(self):
+        return self._job_number
+    @job_number.setter
+    def job_number(self, value):
+        assert value >= 0 and value <= 1000, "Job number out of range"
+        with self._command_lock:
+            self._job_number = value
+            time.sleep(0.1)
+
+    @property
+    def program_number(self):
+        return self._program_number
+    @program_number.setter
+    def program_number(self, value):
+        assert value >= 0 and value <= 255, "Program number out of range"
+        with self._command_lock:
+            self._program_number = value
+            time.sleep(0.1)
+
+    def gas_on(self, duration_seconds):
+        assert duration_seconds < 5
+        with self._command_lock:
+            self._gas_on = True
+            time.sleep(duration_seconds)
+            self._gas_on = False
+
+    def gas_on(self, duration_seconds):
+        assert duration_seconds < 5
+        with self._command_lock:
+            self._gas_on = True
+            time.sleep(duration_seconds)
+            self._gas_on = False
+
+    def wire_inching(self, duration_seconds):
+        assert duration_seconds < 5
+        with self._command_lock:
+            self._wire_inching = True
+            time.sleep(duration_seconds)
+            self._wire_inching = False
+    
+    def wire_retract(self, duration_seconds):
+        assert duration_seconds < 5
+        with self._command_lock:
+            self._wire_retract = True
+            time.sleep(duration_seconds)
+            self._wire_retract = False
+
+    def wire_inching(self, duration_seconds):
+        assert duration_seconds < 5
+        with self._command_lock:
+            self._wire_inching = True
+            time.sleep(duration_seconds)
+            self._wire_inching = False
+
+    @property
+    def arc_length_correction(self):
+        return self._arc_length_correction
+    @arc_length_correction.setter
+    def arc_length_correction(self,value):
+        assert value >= -10 and value <= 10
+        self._arc_length_correction = value
+
+    @property
+    def puls_dynamik_correction(self):
+        return self._puls_dynamik_correction
+    @puls_dynamik_correction.setter
+    def puls_dynamik_correction(self, value):
+        assert value >= -10 and value <= 10
+        self._puls_dynamik_correction = value
+
+    @property
+    def penetration_stabilizer(self):
+        return self._penetration_stabilizer
+    @penetration_stabilizer.setter
+    def penetration_stabilizer(self, value):
+        assert value >= 0 and value <= 10
+        self._penetration_stabilizer = value
+
+    @property
+    def arc_length_stabilizer(self):
+        return self._arc_length_stabilizer
+    @arc_length_stabilizer.setter
+    def arc_length_stabilizer(self, value):
+        assert value >= 0 and value <= 10
+        self._arc_length_stabilizer = value
+
+
+    
+
 def main():
     parser = argparse.ArgumentParser(description="Fronius Welder Power Source")
 
@@ -194,18 +413,21 @@ def main():
 
     welder = WelderImpl(args.welder_ip, None, args.monitor_only)
 
-    with RR.ServerNodeSetup("fronius.welder",60823):
+    try:
+        with RR.ServerNodeSetup("fronius.welder",60823):
 
-        service_ctx = RRN.RegisterService("welder","experimental.fronius.FroniusWelder",welder)
-        welder._start()
+            service_ctx = RRN.RegisterService("welder","experimental.fronius.FroniusWelder",welder)
+            welder._start()
 
-        if args.wait_signal:  
-            #Wait for shutdown signal if running in service mode          
-            print("Press Ctrl-C to quit...")
-            import signal
-            signal.sigwait([signal.SIGTERM,signal.SIGINT])
-        else:
-            #Wait for the user to shutdown the service
-            input("Server started, press enter to quit...")
+            if args.wait_signal:  
+                #Wait for shutdown signal if running in service mode          
+                print("Press Ctrl-C to quit...")
+                import signal
+                signal.sigwait([signal.SIGTERM,signal.SIGINT])
+            else:
+                #Wait for the user to shutdown the service
+                input("Server started, press enter to quit...")
+    finally:
+        welder._stop()
 
 
